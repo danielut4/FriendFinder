32f53fdbd5f6ed299c8fe6a000dcc4e78085cf79
danielut4 <danielwmorgan@gmail.com>
2019-02-06 16:51:12 -0700

    friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,25 @@
 var friendArray = [
+  {
+  "name":"Bob",
+  "photo":"https://i.pinimg.com/236x/ca/ae/f0/caaef0704b9cf8fe2efa0829d30b7215--bobs-burgers-bob-s.jpg",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+  "name": "Tina",
+  "photo": "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-Bobs-Burgers-Dan-Mintz.jpg",
+  "scores": [5,4,4,3,3,5,1,1,2,5]
+  },
+  {
+  "name": "Louise",
+  "photo": "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b9/Louise_render.png/revision/latest/scale-to-width-down/200?cb=20130113075349",
+  "scores": [1,1,1,2,5,4,5,2,1,1]
+  },
+  {
     
+  }
+
+
+
   ]
   
   module.exports = friendArray;
